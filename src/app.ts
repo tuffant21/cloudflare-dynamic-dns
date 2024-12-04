035581e4112a335b625026ba2e6a05cac35d1af9
Anthony <tuffant21@gmail.com>
2024-12-03 21:29:54 -0700

    finalize app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,42 @@
-import axios from 'axios';
 import 'dotenv/config';
+import axios from 'axios';
+import { networkInterfaces } from 'node:os';
 
-async function updateDns() {
+function getIpAddress(): string {
+    const nets = networkInterfaces();
+    const results= Object.create(null);
+    
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name] ?? []) {
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+
+            if (net.family === familyV4Value && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            }
+        }
+    }
+
+    return results["en0"]?.[0] ?? results["eno1"]?.[0] ?? results["eth0"]?.[0] ?? '';
+}
+
+async function updateDns(ipAddress: string): Promise<void> {
     const zoneId = process.env.CLOUDFLARE_ZONE_ID;
     const dnsRecordId = process.env.CLOUDFLARE_DNS_RECORD_ID;
     const authEmail = process.env.CLOUDFLARE_AUTH_EMAIL;
     const authKey = process.env.CLOUDFLARE_AUTH_KEY;
     const domainName = process.env.DOMAIN_NAME;
 
+    const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${dnsRecordId}`;
+
+    console.debug(`[PUT] ${url}`);
+
     try {
         await axios({
             method: 'put',
-            url: `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${dnsRecordId}`,
+            url,
             headers: {
                 'Content-Type': 'application/json',
                 'X-Auth-Email': authEmail,
@@ -20,30 +45,22 @@ async function updateDns() {
             data: {
                 comment: "Domain verification record",
                 name: domainName,
-                proxied: true,
+                proxied: false,
                 settings: {},
                 tags: [],
                 ttl: 3600,
-                content: '',
+                content: ipAddress,
                 type: 'A'
             }
         });
+
+        console.log('Request completed successfully');
     } catch (e) {
         console.error(`There was an error from cloudflare: ${e}`);
+    } finally {
+        process.exit(0);
     }
 }
 
-//curl --request PUT \
-// --url https://api.cloudflare.com/client/v4/zones/zone_id/dns_records/dns_record_id \
-// --header 'Content-Type: application/json' \
-// --header 'X-Auth-Email: ' \
-// --data '{
-// "comment": "Domain verification record",
-// "name": "example.com",
-// "proxied": true,
-// "settings": {},
-// "tags": [],
-// "ttl": 3600,
-// "content": "198.51.100.4",
-// "type": "A"
-// }'
+const ipAddress = getIpAddress();
+updateDns(ipAddress);
